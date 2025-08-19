1d775dfc8b7a8a93f06da830e2d5d36322a7ded4
Hatuos <90397606+Hatuos@users.noreply.github.com>
2025-08-19 16:34:24 +0100

    0.0.1 - Angular 20 migration + angular-fontawesome dependency

--- src/app/social-media/social-media.component.ts
+++ src/app/social-media/social-media.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { socialMediaLinks } from '../../../data';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-social-media',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './social-media.component.html',
   styleUrl: './social-media.component.scss'
 })
 export class SocialMediaComponent {
   socialMediaLinks = socialMediaLinks;
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
 }
